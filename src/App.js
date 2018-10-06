f079be0f93fbdb08fb092d44e076ced934f65677
“azdaves” <azdaveschilling@yahoo.com>
2018-10-05 18:03:28 -0700

    added title change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            WUSD Quiz APP
           </a>
         </header>
       </div>
